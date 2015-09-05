2cc9b4dc2cfb389df786b5b78644e9e62ecd32d7
4Shubby <Wilbur136@hotmail.com>
2015-09-05 18:24:55 -0400

    update

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-/*step one - open sumblime - cmd space
+/*step one - open sublime - cmd space
 
 step 2 - save sublime code to javascript file
 
 step 3 - open terminal; cd Desktop/javascript_algorithms
 
 step 4 - node test.js
+
+push to git hub
+
+- git status in terminal; read output; check for untracked files
+- git add test.js
+- git status; idenitfy changes to be committed
+- git commit -m "message"
+- git push
 
 */
 x=1;
