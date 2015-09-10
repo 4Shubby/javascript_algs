cc4dae70d09fd058996c8ffde9f8ffe4269aa04e
4Shubby <Wilbur136@hotmail.com>
2015-09-10 17:18:02 -0400

    looks at each array and gets largest

---
+++ largest_number.js
@@ -0,0 +1,13 @@
+function largestOfFour(arr) {
+  return arr.map(function(array){
+  	return Math.max.apply(Math, array)
+  });
+  
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+result = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(result);
+
